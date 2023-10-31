1969e6fab21651bb0c704ecc4af48d5f1b953ebf
aldoaquino451 <aldoaquino2909@gmail.com>
2023-10-31 12:30:54 +0100

    [add] newtask al json

--- server.php
+++ server.php
@@ -1,13 +1,33 @@
 <?
 
-$tasks = [
-  "Portare a spasso il cane",
-  "Lavare la macchina",
-  "Fare la spesa",
-  "Cucinare per la cena"
-];
+// richiamo il contenuto del file json
+$tasksString = file_get_contents('list-task.json');
 
+// lo decodifico rendendolo un array
+$tasks = json_decode($tasksString);
+
+
+/* ----------------------------------------- 
+  applico le varie modifiche all'array 
+------------------------------------------ */
+
+// il valore del parametro che gli passo verrà aggiunto al file json, con la condizione che esista e che non sia una stinga vuota 
+if (isset($_POST['taskItem']) && !empty($_POST['taskItem'])) {
+
+  // aggiungo il nuovo task alla lista di task 
+  $newTask = $_POST['taskItem'];
+  $tasks[] = $newTask;
+
+  // codifico la nuova lista per poterla sovrascrivere interamente al precedente contenuto del file json
+  file_put_contents('list-task.json', json_encode($tasks));
+}
+
+/* ------------------------------------ */
+
+
+// lo trasformo in un array json
 header('Content-Type: application/json');
 
+// lo codifico in una stringa json e lo stampo
 echo json_encode($tasks);
 
